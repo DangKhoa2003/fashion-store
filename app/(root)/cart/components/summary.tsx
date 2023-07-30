b3f2669ee595e1591786632ee52faf61050a7e5b
DangKhoa2003 <tranhadangkhoa2003@gmail.com>
2023-07-30 10:02:45 +0700

    Fix Error

--- app/(root)/cart/components/summary.tsx
+++ app/(root)/cart/components/summary.tsx
@@ -27,7 +27,7 @@ const Summary = () => {
       }, [searchParams, removeAll]);
 
       const totalPrice = items.reduce((total, item) => {
-            return total + Number(item.price) * item.quality;
+            return total + Number(item.price);
       }, 0);
 
       const onCheckout = async () => {
