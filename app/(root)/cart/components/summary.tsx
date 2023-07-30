b613d965d9fd6a39c4b8afb117c1ad6b5e180d86
DangKhoa2003 <tranhadangkhoa2003@gmail.com>
2023-07-30 10:06:48 +0700

    Test

--- app/(root)/cart/components/summary.tsx
+++ app/(root)/cart/components/summary.tsx
@@ -27,7 +27,7 @@ const Summary = () => {
       }, [searchParams, removeAll]);
 
       const totalPrice = items.reduce((total, item) => {
-            return total + Number(item.price);
+            return total + Number(item.price) * item.quality;
       }, 0);
 
       const onCheckout = async () => {
