9a3c1c127009c87a12b05d170d2b6595aae2e476
DangKhoa2003 <tranhadangkhoa2003@gmail.com>
2024-02-28 14:27:49 +0700

    fix URL Database

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -8,51 +8,49 @@ import IconButton from './ui/icon-button';
 import { ShoppingBag } from 'lucide-react';
 
 interface MainNavProps {
-      data: Category[];
-      className?: string;
-      darkMode: boolean;
+    data: Category[];
+    className?: string;
+    darkMode: boolean;
 }
 
 const MainNav: React.FC<MainNavProps> = ({ data, className, darkMode }) => {
-      const pathname = usePathname();
+    const pathname = usePathname();
 
-      const routes = data.map((route) => ({
-            href: `/category/${route.id}`,
-            label: route.name,
-            active: pathname === `/category/${route.id}`,
-      }));
+    const routes = data.map((route) => ({
+        href: `/category/${route.id}`,
+        label: route.name,
+        active: pathname === `/category/${route.id}`,
+    }));
 
-      return (
-            <nav
-                  className={`${className} mr-12 items-center space-x-4 lg:space-x-6 z-50`}
-            >
-                  {routes.map((route) => (
-                        <Link
-                              key={route.href}
-                              href={route.href}
-                              className={cn(
-                                    'text-md font-medium tracking-wide uppercase transition-colors ease-linear duration-150',
-                                    route.active && darkMode
-                                          ? 'text-white'
-                                          : 'text-neutral-500',
+    return (
+        <nav
+            className={`${className} mr-12 items-center space-x-4 lg:space-x-6 z-50`}
+        >
+            {routes.map((route) => (
+                <Link
+                    key={route.href}
+                    href={route.href}
+                    className={cn(
+                        'text-md font-medium tracking-wide uppercase transition-colors ease-linear duration-150',
+                        route.active && darkMode
+                            ? 'text-white'
+                            : 'text-neutral-500',
 
-                                    darkMode
-                                          ? 'hover:text-white'
-                                          : 'hover:text-black',
-                              )}
-                        >
-                              {route.label}
-                        </Link>
-                  ))}
+                        darkMode ? 'hover:text-white' : 'hover:text-black',
+                    )}
+                >
+                    {route.label}
+                </Link>
+            ))}
 
-                  <Link href={'/cart'} className="relative top-0 left-0">
-                        <IconButton
-                              className="text-white bg-black p-2 border-none"
-                              icon={<ShoppingBag size={26} />}
-                        />
-                  </Link>
-            </nav>
-      );
+            <Link href={'/cart'} className="relative top-0 left-0">
+                <IconButton
+                    className="p-2 text-white bg-black border-none"
+                    icon={<ShoppingBag size={26} />}
+                />
+            </Link>
+        </nav>
+    );
 };
 
 export default MainNav;
